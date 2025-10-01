6d11998bca2feff0df42a866fcba2f714843f6ea
snehakPV <sneha.sf4154@gmail.com>
2025-10-01 18:57:35 +0530

    Prepared sample and read me for the MAUI Tab View

---
+++ TabViewSample/TabViewSample/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "TabViewSample")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "TabViewSample.Pages")]
---
+++ TabViewSample/TabViewSample/ViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TabViewSample
+{
+    public class TabItemsSourceViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            var handler = PropertyChanged;
+            if (handler != null)
+                handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private ObservableCollection<Model>? tabItems;
+        public ObservableCollection<Model>? TabItems
+        {
+            get { return tabItems; }
+            set
+            {
+                tabItems = value;
+                OnPropertyChanged("TabItems");
+            }
+        }
+        public TabItemsSourceViewModel()
+        {
+            TabItems = new ObservableCollection<Model>();
+            TabItems.Add(new Model() { ImageName = "call.png", SelectedColor = Colors.Red });
+            TabItems.Add(new Model() { ImageName = "contacts.png", SelectedColor = Colors.Gray });
+            TabItems.Add(new Model() { ImageName = "favorite.png", SelectedColor = Colors.Gray });
+        }
+    }
+}
